3bde23764ac57883106a2def01a287a4979006d8
hamza4times <241655835+hamza4times@users.noreply.github.com>
2026-01-26 16:54:36 -0600

    connect and apply font to all text

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const button = document.querySelector('button');
+const button = document.querySelector('#addBookButton');
 const cardsContainer = document.querySelector('#cardsContainer')
 
 //=========================================== EVENT LISTENERS ==================================
