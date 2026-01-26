950ab2ae8bd2ade8e551a0d589854655c187487a
hamza4times <241655835+hamza4times@users.noreply.github.com>
2026-01-26 09:01:11 -0600

    create constructor, add book function, and create unique ID

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-let heading1 = document.querySelector('h1')
+const myLibrary = [];
 
-heading1.style.backgroundColor = 'blue';
+function Book(author, title, pages, status, token) {
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
+
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.status = status;
+  this.token = token;
+}
+
+function addBookToLibrary(author, title, pages, status) {
+  const newBook = new Book(author, title, pages, status, crypto.randomUUID());
+  myLibrary.push(newBook);
+}
